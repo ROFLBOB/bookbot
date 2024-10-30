89bd6b7c5b1d84c62d8dca49d172e940e61e6741
kingpoogie <bmadeley@getgoldn.com>
2024-10-30 17:00:26 -0400

    added count word, count character, and print report functions

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    book_path = "books/frankenstein.txt"
+    with open(book_path) as f:
         file_contents = f.read()
-        #count the number of words in the file
+        print_report(book_path, count_words(file_contents), count_characters(file_contents))
+    
 
-        number_of_words = file_contents.split()
-        print(len(number_of_words))
+def count_words(text_file):
+    #split the string and assign the number of words to a variable, and then return it
+    number_of_words = text_file.split()
+    return len(number_of_words)
+
+def count_characters(text_file):
+    #dictionary
+    letter_count = {}
+    #convert to lower case
+    lowered_text = text_file.lower()
+    #loop through each character in the text file
+    
+    for character in lowered_text:
+        if character in letter_count:
+            letter_count[character] += 1
+        else:
+            letter_count[character] = 1 
+    return letter_count
+
+def print_report(path, total_words, character_counts):
+    
+    print(f"--- Begin report of {path} ---")
+    #get the total count of all words
+    print(f"There werd {total_words} words found in the document.")
+    #print how many times each character was found
+    for k, v in character_counts.items():
+        print(f"The character '{k}' was found {v} times")
+    
 
 main()
